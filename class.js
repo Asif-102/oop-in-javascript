449a61a72db8156a8b797c6ee2ca648ae99c8ecc
Asif2535 <aiqbal517@gmail.com>
2021-04-24 23:32:33 +0600

    basic class before and after es6

--- class.js
+++ class.js
@@ -10,3 +10,10 @@ const ignite = new PenDrive('36GB', 'white', 1300);
 console.log(ignite);
 const oakwood = new PenDrive('4GB', 'black');
 console.log(oakwood);
+
+function StickyNote(color, price){  //before es6
+    this.color = color;
+    this.price = price;
+}
+const yellow = new StickyNote('yellow', 40);
+console.log(yellow);
